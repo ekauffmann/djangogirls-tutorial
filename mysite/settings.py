c1073c1daa69675455181531b63fe7c14ee6e414
Elisa Kauffmann <elisa.kauffmann.f@gmail.com>
2018-06-04 18:26:55 -0400

    add jupyter

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0x!v8-s6sfe=mhex8cl9$!b#9bc1irjm53z=x5=!y6d-x-_*4='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ekauffmann.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ekauffmann.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'blog',
 ]
 
